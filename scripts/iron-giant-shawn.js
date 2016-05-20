a63a4e55b2545fac5fb929e8850536f8ffb32474
Shawn <sbaden11@gmail.com>
2016-05-19 20:03:29 -0700

    shawns simple code

--- scripts/iron-giant-shawn.js
+++ scripts/iron-giant-shawn.js
@@ -7,16 +7,4 @@ module.exports = function(robot) {
     robot.respond(/What's your favorite food?/, function(res) {
        return res.send("I'm a robot--I don't eat food!");
      });
-
-    // robot.respond(/help/, function(msg){​
-    //     return msg.reply("I fix");
-    // });
-
-    // robot.respond(/hello Jacob)\s?\?/i, function(msg){​
-    //     msg.reply("I'm not Jacob. I am the iron giant");
-    // });
-
-    // robot.hear(/bang/i, function(msg){
-    //     msg.send("I am not a gun");
-    // });
 };
