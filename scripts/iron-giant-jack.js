befa1e1aabc849ff8713f2c2cf2aa93e2065a5e7
Shawn <sbaden11@gmail.com>
2016-05-19 19:35:53 -0700

    blah blah

--- scripts/iron-giant-jack.js
+++ scripts/iron-giant-jack.js
@@ -3,11 +3,11 @@
 module.exports = function(robot) {
 
     robot.respond(/What's your favorite drink?/, function(res) {
-       return res.send("I'm a robot--I don't drink!");
+       return res.send("I'm a robot--I do not drink!");
      });
 
     // robot.respond(/help/, function(msg){​
-    //     return msg.reply("I fix");
+    //      return msg.reply("I fix");
     // });
 
     // robot.respond(/hello Jacob)\s?\?/i, function(msg){​
