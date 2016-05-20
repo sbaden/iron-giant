fa7788c997a14c6562bb6182311e3c878fbda613
Shawn <sbaden11@gmail.com>
2016-05-19 20:45:34 -0700

    Hi

--- scripts/iron-giant-shawn.js
+++ scripts/iron-giant-shawn.js
@@ -2,4 +2,7 @@ module.exports = function(robot) {
     robot.respond(/What's your favorite food?/, function(res) {
        return res.send("I'm a robot--I don't eat food!");
      });
+    robot.hear(/Hi!/, function(res){
+        return res.send("No but I wish I was!");
+    });
 };
