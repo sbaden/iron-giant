165bea7dc26dea8c4c1e9bc51a274823d4daf1f9
Shawn <sbaden11@gmail.com>
2016-05-19 19:21:31 -0700

    edit Jacks code

--- scripts/iron-giant-jack.js
+++ scripts/iron-giant-jack.js
@@ -2,8 +2,19 @@
 
 module.exports = function(robot) {
 
-​    robot.respond(/What's your favorite drink?/, function(res) {
+    robot.respond(/What's your favorite drink?/, function(res) {
        return res.send("I'm a robot--I don't drink!");
      });
 
+    // robot.respond(/help/, function(msg){​
+    //     return msg.reply("I fix");
+    // });
+
+    // robot.respond(/hello Jacob)\s?\?/i, function(msg){​
+    //     msg.reply("I'm not Jacob. I am the iron giant");
+    // });
+
+    // robot.hear(/bang/i, function(msg){
+    //     msg.send("I am not a gun");
+    // });
 }
