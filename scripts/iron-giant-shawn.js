9d8d4f8cab88a96a79ecfede1d0e23a72fbf0632
Shawn <sbaden11@gmail.com>
2016-05-19 21:04:17 -0700

    Hogarth

--- scripts/iron-giant-shawn.js
+++ scripts/iron-giant-shawn.js
@@ -5,4 +5,15 @@ module.exports = function(robot) {
     robot.hear(/Hi!/, function(res){
         return res.send("No but I wish I was!");
     });
+
+    var enterReplies = ['Hogarth, is that you?', 'Welcome to the party!'];
+    var leaveReplies = ['Are you still there Hogarth?', "Souls don't die"];
+
+    robot.enter(function(res) {
+        return res.send(res.random(enterReplies));
+      });
+      return robot.leave(function(res) {
+        return res.send(res.random(leaveReplies));
+      });
+
 };
