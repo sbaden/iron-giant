d12c289a9977542594c33d52ac062f4bb93ce850
Shawn <sbaden11@gmail.com>
2016-05-19 18:41:38 -0700

    Another edit

--- scripts/iron-giant-shawn.js
+++ scripts/iron-giant-shawn.js
@@ -3,13 +3,6 @@ var enterReplies = ['Hogarth, is that you?'];
 var leaveReplies = ['Hogarth,you stay, I go. No following', "Souls don't die"];
 
 module.exports = function(robot) {
-    robot.enter(function(res) {
-        return res.send(res.random(enterReplies));
-    });
-
-    robot.leave(function(res) {
-        return res.send(res.random(leaveReplies));
-    });
 
     robot.respond(/help)\s?\?/i, function(msg){​
         msg.reply("I fix");
