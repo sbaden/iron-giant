ae0c655d84aed7c9869bce29bf1179f00999ccb9
Shawn <sbaden11@gmail.com>
2016-05-19 20:42:50 -0700

    clean code

--- scripts/iron-giant-shawn.js
+++ scripts/iron-giant-shawn.js
@@ -1,7 +1,3 @@
-
-// var enterReplies = ['Hogarth, is that you?'];
-// var leaveReplies = ['Hogarth,you stay, I go. No following', "Souls don't die"];
-
 module.exports = function(robot) {
     robot.respond(/What's your favorite food?/, function(res) {
        return res.send("I'm a robot--I don't eat food!");
