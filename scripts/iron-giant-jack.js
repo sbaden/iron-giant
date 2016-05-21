3576eb251e060baecf0bc300b1f2243e74271957
chaptercalc <dutcher.reid@gmail.com>
2016-05-21 13:03:01 -0700

    computerchoice

--- scripts/iron-giant-jack.js
+++ scripts/iron-giant-jack.js
@@ -3,6 +3,10 @@ module.exports = function(robot) {
 
 // Magic8 Ball 
 
+// going to have Magic8
+// RPS
+// War
+
 var magic8 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes",
 "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", 
 "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
@@ -18,16 +22,16 @@ robot.hear(/Magic8/, function(res){
 // get user's input 
 // compair = win or lose
 
-// var computerChoice = Math.random();
-// if (computerChoice < 0.34) {
-//     computerChoice = "Rock";
-// } else if(computerChoice <= 0.67) {
-//     computerChoice = "Paper";
-// } else {
-//     computerChoice = "Scissors";
-// }
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "Rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "Paper";
+} else {
+    computerChoice = "Scissors";
+}
 
-
+res.send(computerChoice);
 
 robot.respond(/hello (.*)/i, function(res){
 	var temp;
@@ -35,6 +39,8 @@ robot.respond(/hello (.*)/i, function(res){
 	return res.reply(temp);
 
 });
+
+
 
 
 robot.respond(/Play RPS: I chose: (.*)/i, function(res) {
