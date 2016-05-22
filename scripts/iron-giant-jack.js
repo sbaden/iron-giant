b68de2809528acda74242c74e811f143ed8aefed
chaptercalc <dutcher.reid@gmail.com>
2016-05-21 18:42:31 -0700

    j

--- scripts/iron-giant-jack.js
+++ scripts/iron-giant-jack.js
@@ -47,34 +47,34 @@ robot.hear(/RPS: (.*)/i, function(res){
 
 	res.reply("YOU: " + temp + " ||| " + "Jacob: " + jacob1);
 	
-var compare = function (choice1, choice2)
-{
-   if (choice1 === choice2)
-   {return "The result is a tie!";}
-   else if (choice1 === "rock"){
-       if (choice2 ==="scissors") 
-       {return "rock wins"} 
-       else 
-       {return "paper wins"}
-       }
-   else if (choice1 === "paper"){
-       if (choice2 ==="rock") 
-       {return "paper wins"} 
-       else 
-       {return "scissors wins"}
-       }
-    else if (choice1 === "scissors"){
-       if (choice2 === "rock") 
-       {return "rock wins"} 
-       else (choice2 === "paper")
-       {return "scissors wins"}
-       }
-    }
+// var compare = function (choice1, choice2)
+// {
+//    if (choice1 === choice2)
+//    {return "The result is a tie!";}
+//    else if (choice1 === "rock"){
+//        if (choice2 ==="scissors") 
+//        {return "rock wins"} 
+//        else 
+//        {return "paper wins"}
+//        }
+//    else if (choice1 === "paper"){
+//        if (choice2 ==="rock") 
+//        {return "paper wins"} 
+//        else 
+//        {return "scissors wins"}
+//        }
+//     else if (choice1 === "scissors"){
+//        if (choice2 === "rock") 
+//        {return "rock wins"} 
+//        else (choice2 === "paper")
+//        {return "scissors wins"}
+//        }
+//     }
 
-compare(userChoice, computerChoice)
+// compare(userChoice, computerChoice)
 
 
-	res.reply(who won);
+// 	res.reply(who won);
 
 });
 
@@ -117,16 +117,16 @@ compare(userChoice, computerChoice)
 // compare(userChoice, computerChoice)
 
 
-var jacobPickss = function(){
-	var computerChoice = Math.random();
-	if (computerChoice <= 0.34) {
-    	return "rock";
-	} else if(computerChoice > 0.34 && computerChoice <= 0.67) {
-    	return "paper";
-	} else {
-    return "scissors";
-	}
-};
+// var jacobPickss = function(){
+// 	var computerChoice = Math.random();
+// 	if (computerChoice <= 0.34) {
+//     	return "rock";
+// 	} else if(computerChoice > 0.34 && computerChoice <= 0.67) {
+//     	return "paper";
+// 	} else {
+//     return "scissors";
+// 	}
+// };
 
 // var jacobPicks = jacobPickss();
 
