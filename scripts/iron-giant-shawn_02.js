9d6380b503f1636a58dabd6bf807fbdb48a8e3f8
Shawn <sbaden11@gmail.com>
2016-05-23 15:22:27 -0700

    code

--- scripts/iron-giant-shawn_02.js
+++ scripts/iron-giant-shawn_02.js
@@ -1,23 +1,21 @@
 module.exports = function(robot) {
   robot.respond(/show users$/i, function(res) {
-    var user;
     var response = "";
     var ref = robot.brain.data.users;
     var results = [];
     var hasProp = {}.hasOwnProperty;
 
     for (key in ref) {
-      if (!hasProp.call(ref, key)) continue;
-        user = ref[key];
+      if (!hasProp.call(ref, key)) {
+        continue;
+        var user = ref[key];
         response += user.id + " " + user.name;
 
-        if (user.email_address) {
-          response += " <" + user.email_address + ">";
-        }
+        if (user.email_address) { response += " <" + user.email_address + ">"; }
 
         results.push(response += "\n");
-      }
+      };
 
       return res.send(results);
-    });
+    };
 };
