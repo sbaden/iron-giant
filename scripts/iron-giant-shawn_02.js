33f39bbbc41693137a7a631a8e1b65bfe70ffcb6
Shawn <sbaden11@gmail.com>
2016-05-23 15:04:14 -0700

    Change msg to res

--- scripts/iron-giant-shawn_02.js
+++ scripts/iron-giant-shawn_02.js
@@ -1,20 +1,20 @@
 var hasProp = {}.hasOwnProperty;
 
 module.exports = function(robot) {
-  return robot.respond(/show users$/i, function(msg) {
+  return robot.respond(/show users$/i, function(res) {
     var key, ref, response, results, user;
     response = "";
     ref = robot.brain.data.users;
     results = [];
     for (key in ref) {
       if (!hasProp.call(ref, key)) continue;
-      user = ref[key];
-      response += user.id + " " + user.name;
-      if (user.email_address) {
-        response += " <" + user.email_address + ">";
+        user = ref[key];
+        response += user.id + " " + user.name;
+        if (user.email_address) {
+          response += " <" + user.email_address + ">";
+        }
+        results.push(response += "\n");
       }
-      results.push(response += "\n");
-    }
-    return results;
-  });
+      return results;
+    });
 };
